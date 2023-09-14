b21401b49adb107c9c42c1b89c174a9739c69df0
coderman2 <okquaye@st.ug.edu.gh>
2023-09-14 21:19:13 +0000

    api-section

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from flask import Flask,render_template,request,redirect, url_for
-
+import requests
 app = Flask(__name__)
+
+@app.route('/get_news',methods = ['GET'])
+def get_news():
+
+    api_url = 'https://newsapi.org/v2/top-headlines'
+    api_key = "6317386d1b9541aebcb2180a0c6fe67a"
+
+    headers = {
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    response = requests.get(api_url,headers=headers)
+
+    if response.status_code == 200:
+        # The request was successful. You can access the API response data using response.json()
+        data = response.json()
+        print(data)
+    else:
+        # The request failed. Handle the error appropriately.
+        print(f"Request failed with status code {response.status_code}")
+
 
 @app.route('/')
 def homepage():
