565da3e3907a6622eef4bd28862d41fd14f5a416
coderman2 <okquaye@st.ug.edu.gh>
2023-09-21 21:07:28 +0000

    homepage

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def news():
-    response = requests.get('https://newsapi.org/v2/top-headlines?q=agriculture&apiKey=6317386d1b9541aebcb2180a0c6fe67a')
-    if response.status_code == 200:
-        # Parse the JSON response
-        news_data = response.json()
+    # response = requests.get('https://newsapi.org/v2/top-headlines?q=bitcoin&apiKey=6317386d1b9541aebcb2180a0c6fe67a')
+    # if response.status_code == 200:
+    #     # Parse the JSON response
+    #     news_data = response.json()
 
-        # Extract the articles from the response
-        articles = news_data.get('articles', [])
+    #     # Extract the articles from the response
+    #     articles = news_data.get('articles', [])
 
-        return render_template('hompage.html', articles=articles)
-    else:
-        return 'Error fetching news'
+        return render_template('hompage.html')
+    # else:
+    #     return 'Error fetching news'
 
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0')
