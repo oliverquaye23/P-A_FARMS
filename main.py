52daee382f96e561e79685f375936dbcdffd2d5c
coderman2 <okquaye@st.ug.edu.gh>
2024-01-06 16:26:59 +0000

    error check

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from flask import Flask,render_template,request,redirect, url_for
-from newsapi import NewsApiClient
 import requests
 app = Flask(__name__)
-
-# newsapi = NewsApiClient(api_key='6317386d1b9541aebcb2180a0c6fe67a')
-# top_headlines = newsapi.get_top_headlines(category='business')
-# @app.route('/')
-# def homepage():
-#     return render_template('hompage.html')
 
 @app.route('/')
 def news():
